981b1388123e2a98ce04576549df3acfa6c6c91c
bcoggins78 <bcoggins78@gmail.com>
2019-03-22 20:40:18 -0400

    Initial push of files, initialize Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBnHwLO9LueMKMYvhgRUK2IoyuD5LV7VGA",
+    authDomain: "trainscheduler-dbaca.firebaseapp.com",
+    databaseURL: "https://trainscheduler-dbaca.firebaseio.com",
+    projectId: "trainscheduler-dbaca",
+    storageBucket: "trainscheduler-dbaca.appspot.com",
+    messagingSenderId: "480317516197"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
