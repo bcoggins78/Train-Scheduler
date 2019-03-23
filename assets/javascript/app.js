2c4ae0a7d4436a6eb89771932fad6b78124d93a1
bcoggins78 <bcoggins78@gmail.com>
2019-03-22 23:34:55 -0400

    Added majority of javascript / jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,68 @@ var config = {
   var database = firebase.database();
 
 
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainTime = $("#firsttime-input").val().trim();  // <--- Convert to Military Time
+    var trainFreq = $("#frequency-input").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDest,
+        time: trainTime,
+        frequency: trainFreq,
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+
+    alert("Train successfully added");
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#firsttime-input").val("");
+    $("#frequency-input").val("");
+
+  });
+
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFreq = childSnapshot.val().frequency
+
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainTime);
+    console.log(trainFreq);
+
+    // Calculate Next Arrival
+    var nextArrival
+
+    // Calculate Minutes Away
+    var minutesAway
+    
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway)
+    );
+
+        $("#train-table > tbody").append(newRow);
+  });
+
+
 
 
 
